ecc8371d024cd28d1af6ce2222fd1394e1ebac05
Laurent REYNAUD <72187493+LeRoiFou@users.noreply.github.com>
2022-03-16 21:04:47 +0100

    Add files via upload

---
+++ index001_GoInstallation.go
@@ -0,0 +1,17 @@
+// Lien : https://www.youtube.com/watch?v=Mz8WjlfHBoE
+// Installation du programme go
+// Éditeur : Laurent REYNAUD
+// Date : 16-03-22
+
+package main // package principal
+
+// Importation d'autres packages : fmt -> affichage dans la console
+import (
+	"fmt"
+)
+
+func main() {
+
+	fmt.Println("Hello World!")
+
+}
